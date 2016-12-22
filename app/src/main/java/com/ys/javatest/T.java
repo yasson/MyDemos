0f6ca4c16c80b6bfeed6ef9faf88a17592f7043f
nufeng <nufeng@mogujie.com>
2016-12-22 15:45:24 +0800

    a

---
+++ app/src/main/java/com/ys/javatest/T.java
@@ -0,0 +1,12 @@
+package com.ys.javatest;
+
+/**
+ * Created by nufeng on 2016/12/22.
+ */
+
+public class T {
+    public static void main(String[] args){
+        Object a = new Object();
+        Object b = a;
+    }
+}
